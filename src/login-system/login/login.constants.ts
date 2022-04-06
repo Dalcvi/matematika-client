19e9a05219af839c551ef3d41abe32defd801db8
Dalius Cvilikas <daliusc@wix.com>
2022-04-06 03:22:41 +0300

    updated to our backend

--- src/login-system/login/login.constants.ts
+++ src/login-system/login/login.constants.ts
@@ -1,3 +1,3 @@
 export const LOGIN_LOADING_STATE_ID = 'login-system/login';
 export const SUCCESSFUL_LOGIN = 'Successfully logged in!';
-export const LOGIN_URI = '/api/auth/login';
+export const LOGIN_URI = '/auth/login';
